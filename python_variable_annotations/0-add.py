772f62e35a0090132aa6b694e29b36e52a744962
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-02 15:32:13 -0600

    task 0: pycodestyle ,'P

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
+
+"""
+write a type-annotated function called add
+that takes two floats called a and b as args
+and returns their sum as a float
+"""
+
+
 def add(a: float, b: float) -> float:
+    """ returns the sum of two floats """
     return a + b
