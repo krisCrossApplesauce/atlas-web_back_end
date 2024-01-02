e4252066f2898502e6d2e64f1027cbed54ade593
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-02 15:58:47 -0600

    task 3 :)

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+write a type-annotated function to_str
+that takes a float called n as an arg
+and returns the string representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """ returns the result of converting a float into a string """
+    return str(n)
