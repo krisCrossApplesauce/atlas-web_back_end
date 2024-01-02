ee199e5e0364b366b1c03ca31a1d25afeae54cbb
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-02 15:39:39 -0600

    task 1 :)

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+write a type-annotated function called concat
+that takes two strings called str1 and str2 as args
+and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ returns the concatenated string of two strings """
+    return str1 + str2
