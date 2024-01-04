ceb15c8896b13f944499cf4b6823918bcc20202b
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-04 15:26:47 -0600

    task 9: line too long >,'

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -7,5 +7,6 @@ with the appropriate types
 import typing
 
 
-def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+def element_length(lst: typing.Iterable[typing.Sequence]) \
+        -> typing.List[typing.Tuple[typing.Sequence, int]]:
     return [(i, len(i)) for i in lst]
