275ff12dc576418848e263c6923f3c4db6e2fa13
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-04 15:51:38 -0600

    I'm too distracted: what would you do for an empanada

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+write an asynchronous coroutine named wait_random--
+that takes in an int arg called max_delay, with a default value of 10
+--that waits for a random delay between 0
+and max_delay (included and float value) seconds
+and eventually returns it
+"""
+import random
+
+
+def wait_random(max_delay):
+    """ """
