3fa7da5101e663216e01f9204b04fd112b7b6e80
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-04 14:44:18 -0600

    task 6 :P

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+write a type-annotated function called sum_mixed_list
+which takes a list called mxd_lst of ints and floats
+and returns their sum as a float
+"""
+import typing
+
+
+def sum_mixed_list(mxd_lst: typing.List[typing.Union[int, float]]) -> float:
+    """ returns sum of the numbers of a list as a float """
+    n: float = 0
+    for i in mxd_lst:
+        if type(i) is int:
+            n += float(i)
+        else:
+            n += i
+    return n
