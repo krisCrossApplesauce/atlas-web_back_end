d9bc02e47a13a67a6e2525f59adad202458eeb26
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-04 15:10:40 -0600

    task 8 :D

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+write a type-annotated function called make_multiplier
+that takes a float called multiplier as an arg
+and returns a function that multiplies a float by multiplier
+"""
+import typing
+
+
+def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
+    """ a function that returns a function that takes a float to be multiplied by the multiplier """
+    def infunc(f: float) -> float:
+        return f * multiplier
+
+    return infunc
