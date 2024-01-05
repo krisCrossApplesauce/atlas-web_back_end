282d84330b43ccc8e663aca1fd46126ef522c096
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-05 16:00:39 -0600

    began the file for 2-measure_runtime.py

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+ipmort async_comprehension from the prev file
+and write a coroutine called measure_runtime
+that will execute async_comprehension four times in parallel
+using asyncio.gather
+
+measure_runtime should measure the total runtime and return it
+"""
+import asyncio
+import typing
+
+
+async def measure_runtime():
