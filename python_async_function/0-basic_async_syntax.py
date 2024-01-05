308182eed71997062bee5570ad7d137c073d6210
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-05 13:06:55 -0600

    task 0 :)

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -8,7 +8,13 @@ and max_delay (included and float value) seconds
 and eventually returns it
 """
 import random
+import asyncio
 
 
-def wait_random(max_delay):
-    """ """
+async def wait_random(max_delay: int = 10) -> float:
+    """ delays for a random number of seconds
+    within the range of 0 and max_delay
+    before returning the value of the delay (a float) """
+    d = random.uniform(0, max_delay)
+    await asyncio.sleep(d)
+    return d
--- python_async_function/0-main.py
+++ python_async_function/0-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-ipmort asyncio
+import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
