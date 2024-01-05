20d0508730ce866fd87f8cb3edc7a25aceedd8bb
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-05 13:44:44 -0600

    1-concurrent_coroutines.py: pycodestyle ,'P line too long, also got rid of setting max_delay to 10 in the hopes that it fixes check 4 bc it never specified that I should/had to

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -13,8 +13,8 @@ import typing
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> typing.List[float]:
-    """ runs wait_random n times and then returns a list of wait_random's results """
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
+    """ runs wait_random n times and returns a list of the results """
     lst = []
     i = 0
 
