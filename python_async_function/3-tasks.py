9cd42838711b4ea198e0c36edd507c2002b48e69
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-05 14:57:42 -0600

    task 3 :P I'm suspicious it won't be this easy, but we'll see

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+import wait_random from 0-basic_async_syntax.
+write a (regular, non-async) func called task_wait_random
+that takes an int called max_delay
+and returns a asyncio.Task
+"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """ returns an asyncio.Task """
+    return asyncio.Task(wait_random(max_delay))
