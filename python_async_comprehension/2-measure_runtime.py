e6a7c057bdb77c91c04139ecc2b705e66ee570bc
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-06 15:06:15 -0600

    task 2: single line for loops is very new to me, very odd, it seems that what you put before the loop is what is being looped and is what's to be added to the list, can use _ (in 'for _ in range(4)') when that var isn't going to be used for anything; also, I don't know what the purpose of * is

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,9 +9,16 @@ using asyncio.gather
 measure_runtime should measure the total runtime and return it
 """
 import asyncio
+import time
 import typing
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime():
     """ already described in the comment at the beginning of the file """
+    before = time.time()
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    after = time.time()
+    total_time = after - before
 
+    return total_time
