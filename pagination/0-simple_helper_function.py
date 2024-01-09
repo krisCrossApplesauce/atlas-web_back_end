dfc3f6418d70e6502cef6855d2e92edce503ecc2
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-09 17:12:52 -0600

    task 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -5,5 +5,11 @@ Write a function named index_range
 that takes two int args called page and page_size.
 
 The func should return a tuple of size two
-containing 
+containing a start index and an end index
+corresponding to the range of indexes to return in a list
+for those particular pagination parameters.
 """
+
+def index_range(page, page_size) -> tuple:
+    """ a func that returns a tuple """
+    return ((page - 1) * page_size, page * page_size)
