e55bad59e43041f4b92dcec212694dd3cb48d8af
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-09 13:07:42 -0600

    0: PEP8, != -> is not

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -25,7 +25,7 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """ add an item in the cache """
-        if key != None and item != None:
+        if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
