b3dd185cdd235603e3f158bfdde81d074de3fd95
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-09 17:49:35 -0600

    gonna be done for today :P nightmare nightmare nightmare

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+"""
+Copy index_range from the prev task and the following class into your code.
+
+In the given class, implement a method named get_page
+that takes two int args:
+one called page, with a default value of 1
+and another called page_size, with a default value of 10
+	>  Must use the csv file (Popular_Baby_Names.csv)
+	>  Use assert to verify that both args are ints greater than 0
+	>  Use index_range to find the correct indexes to paginate the dataset correctly
+	   and return the appropriate page of the dataset (i.e. the correct list of rows)
+	>  If the input args are out of range for the dataset,
+	   an empty list should be returned
+"""
+
+
+def index_range(page, page_size) -> tuple:
+	""" a func that returns a tuple """
+	return ((page - 1) * page_size, page * page_size)
+
+
+import csv
+import math
+from typing import List
+
+
+class Server:
+	""" Server class to paginate a database of popular baby names
+	"""
+	DATA_FILE = "Popular_Baby_Names.csv"
+
+	def __init__(self):
+		self.__dataset = None
+
+	def dataset(self) -> List[List]:
+		"""Cached dataset
+		"""
+		if self.__dataset is None:
+			with open(self.DATA_FILE) as f:
+				reader = csv.reader(f)
+				dataset = [row for row in reader]
+			self.__dataset = dataset[1:]
+
+		return self.__dataset
+
+	def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+		pass
