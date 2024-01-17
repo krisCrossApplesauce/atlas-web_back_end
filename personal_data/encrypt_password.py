0c32173ed92d3f81b95fa68c6ca6ed8db0b593ce
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-17 15:41:15 -0600

    task 5?

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Implement a func called hash_password
+that expects one string arg named password
+and returns a salted, hashed password, which is a byte str
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    """ hashes a password I guess """
+    bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(bytes, salt)
