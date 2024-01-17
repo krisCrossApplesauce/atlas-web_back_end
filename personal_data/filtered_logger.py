2f25bd474e17905e879f54b1f747080c5b88dfcd
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-17 12:40:37 -0600

    task 0: .* -> .*? bc * is greedy and will include everything until the last instance of the next char, but adding ? makes it lazy and only include everything until the FIRST instance of the next char

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,11 +4,12 @@
 write a func called filter_datum that returns the log message obfuscated:
     > Arguments:
         >  fields: a list of strings representing all fields to obfuscate
-        >  redaction: a string representing what the field will be obfuscated by
-        >  message: a string representing the log line
-        >  separator: a string representing which character is separating 
+        >  redaction: a str representing what the field will be obfuscated by
+        >  message: a str representing the log line
+        >  separator: a str representing which character is separating
            all the fields in the log line (message)
-    > The func should use a regex to replace occurrences of certain field values
+    > The func should use a regex
+      to replace occurrences of certain field values
     > filter_datum should be less than 5 lines long
       and use re.sub to perform the substitution with a single regex
 """
@@ -20,6 +21,6 @@ def filter_datum(fields: typing.List[str],
                  redaction: str, message: str, separator: str) -> str:
     """ a function that does something """
     for f in fields:
-        message = re.sub(fr'{f}=.*{separator}',
+        message = re.sub(fr'{f}=.*?{separator}',
                          fr'{f}={redaction}{separator}', message)
     return message
