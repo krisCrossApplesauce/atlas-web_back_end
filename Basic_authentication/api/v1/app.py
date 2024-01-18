1b430e3c5ac9f0400195b46eb54392870d0a075e
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-17 19:55:34 -0600

    task 5 :P

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -12,7 +12,11 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
+if os.getenv("AUTH_TYPE") == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -33,6 +37,22 @@ def forbidden(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """ before request """
+    if auth is not None:
+        this_list = ['/api/v1/status/',
+                     '/api/v1/unauthorized/',
+                     '/api/v1/forbidden/']
+        if auth.require_auth(request.path, this_list):
+            if auth.authorization_header(request) is None:
+                raise abort(401)
+            if auth.current_user(request) is None:
+                raise abort(403)
+    return
+
 
 
 if __name__ == "__main__":
--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -10,6 +10,9 @@ from typing import List, TypeVar
 class Auth():
     """ a class called Auth """
 
+    def __init__(self):
+        """ initializes self """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require auth method """
         if path is None or excluded_paths is None or len(excluded_paths) is 0:
@@ -23,7 +26,9 @@ class Auth():
 
     def authorization_header(self, request=None) -> str:
         """ authorization header method """
-        return None
+        if request is None or "Authorization" not in request.headers:
+            return None
+        return request["Authorization"]
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ current user method """
