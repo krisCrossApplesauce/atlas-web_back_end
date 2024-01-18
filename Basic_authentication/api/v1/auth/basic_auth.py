d95dfffb6eb6bfb9fc5b1bdb4d9b4d920c1a948a
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-17 20:46:08 -0600

    task 7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,3 +8,15 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ basic auth """
+
+    def __init__(self):
+        """ initialize stuff """
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ ridiculously long method name but ok I guess """
+        if authorization_header is None \
+            or type(authorization_header) is not str \
+                or authorization_header[:6] != "Basic ":
+            return None
+        return authorization_header[6:]
