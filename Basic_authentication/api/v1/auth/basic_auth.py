f861177f367ff4bfd5c3993ecfba312908a44a48
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-17 21:07:34 -0600

    task 9 :)

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -34,3 +34,14 @@ class BasicAuth(Auth):
                                     ).decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """ returns the user email and password from the b64 decoded value """
+        if decoded_base64_authorization_header is None \
+            or type(decoded_base64_authorization_header) is not str \
+                or ":" not in decoded_base64_authorization_header:
+            return None, None
+        email, password = decoded_base64_authorization_header.split(":", 1)
+        return email, password
