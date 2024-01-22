0b939274e5f79078ad8b456145ddf9a373e48621
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-22 11:47:31 -0600

    Session_authentication: ok, it should be all good now

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -17,7 +17,7 @@ auth = None
 if os.getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif os.getenv("AUTH_TYPE") == "basic_auth":
+if os.getenv("AUTH_TYPE") == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
