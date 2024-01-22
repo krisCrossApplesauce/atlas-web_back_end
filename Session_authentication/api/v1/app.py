e72a6ec59ef2ffef7fffa4f7f1da97817fcbad14
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-22 11:32:33 -0600

    trying to fix Basic_authentication so I can do Session_authentication

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -17,7 +17,7 @@ auth = None
 if os.getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-if os.getenv("AUTH_TYPE") == "basic_auth":
+elif os.getenv("AUTH_TYPE") == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
