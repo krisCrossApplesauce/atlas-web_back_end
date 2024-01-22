29007ffdd72810e231260d4904c40102c96c720e
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-21 21:14:34 -0600

    no chance that works; the order of the errors in the file changed to be in order like: 404, 403, 401 instead of 404, 401, 403

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -29,18 +29,18 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """ Unauthorized handler
-    """
-    return jsonify({"error": "Unauthorized"}), 401
-
-
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.before_request
