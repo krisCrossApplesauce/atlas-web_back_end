653f68b1c3e580aec543b4326e990d052d935c51
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-23 13:27:25 -0600

    task 6 :P

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -24,3 +24,8 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ (overload) that returns a User instance based on a cookie value """
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
+        return User.get(user_id)
