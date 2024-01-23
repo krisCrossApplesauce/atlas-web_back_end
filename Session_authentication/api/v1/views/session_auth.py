d26844ce62694f875a02bf5f39e79253acb5b504
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-23 14:48:55 -0600

    7: ?...

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """ handles all routes for the Session authentication """
 from api.v1.views import app_views
+from flask import make_response
 from models.user import User
 import json
 
 
-@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+@app_views.route('/auth_session/login', methods=['POST'],
+                 strict_slashes=False)
 def login():
     """ it's not really telling me what this is supposed to do """
     email = request.form.get("email")
@@ -20,5 +22,8 @@ def login():
     if not user.is_valid_password(password):
         return { "error": "wrong password" }, 401
     from api.v1.app import auth
-    session_id = auth.create_session(user)
-    return user.to_json()
+    session_id = auth.create_session(user.id)
+    response = user.to_json()
+    response = make_response(response)
+    response.set_cookie(os.getenv('SESSION_NAME'), session_id)
+    return response
