e66f099ec7e19e27226d015f257cdadbb551ea74
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-23 12:45:15 -0600

    task 3

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,7 +3,18 @@
 Create a class called SessionAuth that inherits from Auth
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ session auth """
+
+    user_id_by_session_id = []
+
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a Session ID for a user_id """
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = kwargs.get('id', str(uuid.uuid4()))
+        user_id_by_session_id[session_id] = user_id
+        return session_id
