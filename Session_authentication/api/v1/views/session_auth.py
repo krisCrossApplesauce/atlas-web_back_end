4c5299050e89938dae9192e036e6c6eac67a1d4b
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-23 15:06:01 -0600

    7: I don't see why this isn't working

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """ handles all routes for the Session authentication """
 from api.v1.views import app_views
-from flask import jsonify, make_response
+from flask import request, jsonify, make_response
 from models.user import User
-import json
 
 
 @app_views.route('/auth_session/login', methods=['POST'],
@@ -24,7 +23,6 @@ def login():
         return jsonify({ "error": "wrong password" }), 401
 
     from api.v1.app import auth
-
     session_id = auth.create_session(user.id)
     response = make_response(jsonify(user.to_json()))
     response.set_cookie(os.getenv('SESSION_NAME'), session_id)
