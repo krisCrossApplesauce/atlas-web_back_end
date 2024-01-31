cfdfa5a14a494210eded9a261195762dff97f813
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-31 15:38:35 -0600

    0: I think this should work now, it's returning the index file as a template, totally skipped over that work when reading through the task description

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 """ setup a basic Flask app """
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
+def hello_world():
+    """ returns the index.html template """
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run()
