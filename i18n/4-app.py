e850ad5925b2783143c2730a61f85b797e2be7e6
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-31 17:00:03 -0600

    4: going home now bc it's getting late, but I copied the files so I have the files for task 4 at least :P

---
+++ i18n/4-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" setup a basic Flask app """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    """ to configure language and timezone in the app """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ ok now I make a get_locale func """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def hello_world():
+    """ returns the index.html template """
+    from flask_babel import gettext as _
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
