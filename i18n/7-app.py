5f71755638c8f45915e630caa97fe83b4148259c
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-31 18:52:42 -0600

    7: should be good I think?

---
+++ i18n/7-app.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+""" setup a basic Flask app """
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    """ to configure language and timezone in the app """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ ok now I make a get_locale func """
+    locale = request.args['locale']
+    if locale is not None and locale in app.config['LANGUAGES']:
+        return locale
+    if g.user.locale is not None:
+        return g.user.locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@babel.timezoneselector
+def get_timezone():
+    """ same as get_locale but with timezones now :P """
+    import pytz
+    timezone = request.args['timezone']
+    if timezone is not None and timezone in pytz.timezone:
+        return timezone
+    if g.user.timezone is not None:
+        return g.user.timezone
+    return "UTC"
+
+
+@app.route('/')
+def hello_world():
+    """ returns the index.html template """
+    from flask_babel import gettext as _
+    return render_template('7-index.html')
+
+
+def get_user():
+    """ returns a user dict or None if ID not found or login_as not passed """
+    id = int(request.args['login_as'])
+    if id is not None and id in users:
+        return users[id]
+    return None
+
+
+@app.before_request
+def before_request():
+    """ gets user and sets it as a global(?) on flask.g.user """
+    g.user = get_user()
+
+
+if __name__ == '__main__':
+    app.run()
