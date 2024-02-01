7759710f6a785a2d9795235bfe189b5ac2419851
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-31 18:01:47 -0600

    4: ok, I wasn't sure if request.accept_languages.best_matchh() was the way to check if request.args['locale'] was an acceptable language or if I was supposed to check with the languages I had or whatever, y'know? basically, now I know that request.args['locale'] and request.accept_languages are both to get the users language or whatever ,'P

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -21,8 +21,9 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """ ok now I make a get_locale func """
-    if request.args['locale'] is not None:
-        return request.accept_languages.best_match(request.args['locale'])
+    locale = request.args['locale']
+    if locale is not None and locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
