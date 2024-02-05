e45bf7f1794ddac4649b9ede3bba9b44c2226337
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-05 16:22:50 -0600

    task 3 ,'/ I forgot the () to self.a_method in a_property

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -2,6 +2,7 @@
 """ unittest for utils.py """
 access_nested_map = __import__('utils').access_nested_map
 get_json = __import__('utils').get_json
+memoize = __import__('utils').memoize
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
@@ -42,7 +43,28 @@ class TestGetJson(unittest.TestCase):
         mock = Mock()
         mock.json.return_value = test_payload
 
-        with patch('requests.get', return_value=mock) as mock_get:
+        with patch('requests.get', return_value=mock) as get:
             result = get_json(test_url)
             self.assertEqual(result, test_payload)
-            mock_get.assert_called_once_with(test_url)
+            get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """ unittest for memoize from utils.py """
+
+    def test_memoize(self):
+        """ tests memoize """
+
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as a_method:
+            test_instance = TestClass()
+            self.assertEqual(test_instance.a_property, 42)
+            self.assertEqual(test_instance.a_property, 42)
+            a_method.assert_called_once()
