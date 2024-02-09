614cff9854f246859ac270d22e291a533457e629
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-09 13:01:07 -0600

    task 1 :)

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Cache class """
 import redis
-import typing
 import uuid
+from typing import Callable, Union
 
 
 class Cache():
@@ -13,8 +13,23 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: typing.Union[str, bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """ takes a data arg and returns a str """
         key = str(uuid.uuid4())
         self._redis[key] = data
         return key
+
+    def get(self, key: str, fn: Callable):
+        """ converts data back to desired form """
+        b_data = self._redis.get(key)
+        if callable(fn):
+            return fn(b_data)
+        return b_data
+
+    def get_str(self, key: str):
+        """ automatically parametrize get method w/ str conversion func """
+        return self.get(key, str)
+
+    def get_int(self, key: str):
+        """ automatically parametrize get method w/ int conversion func """
+        return self.get(key, int)
