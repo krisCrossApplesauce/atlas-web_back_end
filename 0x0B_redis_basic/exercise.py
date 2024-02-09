f4cdef756009313f70fc9412e30c570d6c7c3be5
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-09 12:37:53 -0600

    task 0 :)

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Cache class """
+import redis
+import uuid
+
+
+class Cache():
+    """ Cache class """
+
+    def __init__(self):
+        """ initializes an instance """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: [str, bytes, int, float]) -> str:
+        """ takes a data arg and returns a str """
+        key = str(uuid.uuid4())
+        self._redis[key] = data
+        return key
