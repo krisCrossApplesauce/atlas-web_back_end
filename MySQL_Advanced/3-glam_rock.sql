0539903ba6dd4417744ca8390a73c03bd0d61104
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-12 15:11:59 -0600

    3: the sandbox doesn't work what is the point

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -3,5 +3,5 @@
 -- ranked by their longevity
 SELECT band_name, SUBTRACT(IFNULL(split, 2024), formed) as lifespan
 FROM metal_bands
-WHERE metal_bands.style LIKE 'Glam rock%'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
