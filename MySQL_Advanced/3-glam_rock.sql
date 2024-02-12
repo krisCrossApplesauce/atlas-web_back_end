c0fb8ae4df914a08fb5038fb9104a0d96e0b3c45
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-12 14:30:45 -0600

    task 3 :P let's see if it works

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- write an SQL script that lists all bands
+-- with Glam rock as their main style,
+-- ranked by their longevity
+SELECT band_name, (split - formed) as lifespan
+FROM metal_bands
+WHERE style = 'Glam rock';
