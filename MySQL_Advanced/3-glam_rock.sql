0cc686d59c9496cf9d9872f7643e145f0bf5ac97
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-12 14:44:26 -0600

    forgot that split can be NULL so have to replace value with the current year ,'P

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- write an SQL script that lists all bands
 -- with Glam rock as their main style,
 -- ranked by their longevity
-SELECT band_name, SUBTRACT(split, formed) as lifespan
+SELECT band_name, SUBTRACT(IFNULL(split, 2024), formed) as lifespan
 FROM metal_bands
 WHERE metal_bands.style LIKE 'Glam rock%';
