2d2f26f20a193a47bef161b4fe54f30d5fef37da
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-12 14:38:25 -0600

    3: ok, style -> metal_bands.style, an used SUBTRACT

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- write an SQL script that lists all bands
 -- with Glam rock as their main style,
 -- ranked by their longevity
-SELECT band_name, (split - formed) as lifespan
+SELECT band_name, SUBTRACT(split, formed) as lifespan
 FROM metal_bands
-WHERE style LIKE 'Glam rock%';
+WHERE metal_bands.style LIKE 'Glam rock%';
