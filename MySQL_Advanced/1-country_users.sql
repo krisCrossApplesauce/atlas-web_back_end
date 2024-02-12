1e0a2c589b223d08263708599af20f7fff11b24b
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-12 13:14:15 -0600

    task 1: hm

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,11 @@
+-- write an SQL script that creates a table called users that:
+-- contains the attributes: id, email, name, and country
+-- doesn't fail if table already exists
+-- is executable on any database
+CREATE TABLE IF NOT EXISTS users (
+    id int NOT NULL AUTO_INCREMENT,
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255),
+    country ENUM ('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY (id)
+)
