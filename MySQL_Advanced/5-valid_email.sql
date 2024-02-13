64eca37c70b2cfb580d2ae32d81da5dc97eda2f7
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-13 13:02:23 -0600

    5: THEN END IF??

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -4,5 +4,6 @@
 CREATE TRIGGER change_valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
-IF OLD.email != NEW.email
-    SET NEW.valid_email = 0;
+IF OLD.email != NEW.email THEN
+    SET NEW.valid_email = 0
+END IF;
