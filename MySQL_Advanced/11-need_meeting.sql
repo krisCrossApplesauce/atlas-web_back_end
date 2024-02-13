08cc926e672d685ce7231bb5a418179d0013af2f
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-13 15:49:05 -0600

    11: there we go, there's a parameter that allows you to specify by what measurement of time you want DATEDIFF to use

--- MySQL_Advanced/11-need_meeting.sql
+++ MySQL_Advanced/11-need_meeting.sql
@@ -4,4 +4,4 @@
 CREATE VIEW need_meeting AS
 SELECT name
 FROM students
-WHERE score <= 80 AND (last_meeting IS NULL OR DATEDIFF(last_meeting, CURDATE()) > 30);
+WHERE score <= 80 AND (last_meeting IS NULL OR DATEDIFF(month, last_meeting, CURDATE()) > 0);
