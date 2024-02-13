4ae8e9338dd0b128a1cc51ccf1e91fe98c722560
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-13 13:44:18 -0600

    6: woops, NOT only had [] around it in the example I was looking at bc it was to imply that it was optional, NOT doesn't work with [] around it

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -4,7 +4,7 @@ delimiter //
 CREATE PROCEDURE AddBonus
 (IN user_id INT, IN project_name VARCHAR(255), IN score FLOAT)
 BEGIN
-    IF [NOT] EXISTS (SELECT name FROM projects WHERE projects.name = project_name)
+    IF NOT EXISTS (SELECT name FROM projects WHERE projects.name = project_name)
         INSERT INTO projects (name) VALUES (project_name);
     INSERT INTO corrections
     VALUES (user_id, SELECT id FROM projects WHERE name = project_name, score);
