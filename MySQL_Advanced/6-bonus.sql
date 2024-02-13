33feda70674ee84f1b17dd15ac184de0d5d011ad
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-13 13:34:11 -0600

    6: I change IN to NOT IN (like it should've been) as if it's really gonna work

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -4,7 +4,7 @@ delimiter //
 CREATE PROCEDURE AddBonus
 (IN user_id INT, IN project_name VARCHAR(255), IN score FLOAT)
 BEGIN
-    IF project_name IN projects.name
+    IF project_name NOT IN projects.name
         INSERT INTO projects (name) VALUES (project_name);
     INSERT INTO corrections
     VALUES (user_id, projects.id WHERE projects.name = project_name, score);
