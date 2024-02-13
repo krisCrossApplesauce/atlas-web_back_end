71b3bd142e8a730944d8777d7a485a568ff08a5d
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-13 13:30:05 -0600

    task 6?... just kinda making stuff up tbh

---
+++ MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,12 @@
+-- write an SQL script that creates a stored procedure called AddBonus
+-- that adds a new correction for a student
+delimiter //
+CREATE PROCEDURE AddBonus
+(IN user_id INT, IN project_name VARCHAR(255), IN score FLOAT)
+BEGIN
+    IF project_name IN projects.name
+        INSERT INTO projects (name) VALUES (project_name);
+    INSERT INTO corrections
+    VALUES (user_id, projects.id WHERE projects.name = project_name, score);
+END//
+delimiter;
