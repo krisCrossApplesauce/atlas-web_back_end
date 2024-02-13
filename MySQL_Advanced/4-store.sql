dbd8617d03acf26c96864651e53cba8a9cfd7771
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-13 12:20:52 -0600

    4?...

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -3,5 +3,5 @@
 -- quantity in the table items can be negative
 CREATE TRIGGER items_trigger AFTER INSERT INTO orders
 FOR EACH ROW SET UPDATE items
-SET quantity = quantity - orders.number
+SET quantity = (quantity - orders.number)
 WHERE name = orders.item_name
