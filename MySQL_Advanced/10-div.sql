30a34a6bab0c4d34bf11c77665336862d775de01
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-13 15:24:31 -0600

    10: I don't know what's wrong with this

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -2,16 +2,13 @@
 -- that divides (and returns) the first number by the second number
 -- or returns 0 if the second number is equal to 0
 DELIMITER//
-CREATE FUNCTION SafeDiv(
-    a INT,
-    b INT
-) RETURNS FLOAT
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
 BEGIN
-    DECLARE @c AS INT = 0
-    IF b != 0 THEN
-        SET @c = (a / b);
+    IF b = 0 THEN
+        RETURN 0;
     END IF;
-    RETURN @c;
+    RETURN a / b;
 END;
 //
 
