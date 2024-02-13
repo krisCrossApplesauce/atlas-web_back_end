5dc02cca51885f475320edc9518d927053299f36
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-13 15:51:11 -0600

    11: maybe it returns decimals? so really any period of time would be greater than 0, so gotta specify >= 1 instead

--- MySQL_Advanced/11-need_meeting.sql
+++ MySQL_Advanced/11-need_meeting.sql
@@ -4,4 +4,4 @@
 CREATE VIEW need_meeting AS
 SELECT name
 FROM students
-WHERE score <= 80 AND (last_meeting IS NULL OR DATEDIFF(month, last_meeting, CURDATE()) > 0);
+WHERE score <= 80 AND (last_meeting IS NULL OR DATEDIFF(month, last_meeting, CURDATE()) >= 1);
