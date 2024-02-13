f277c1d0098b30928ff0b6028650465d11ded3cf
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-13 14:16:41 -0600

    7: why would you have me name the input the same thing as the attribute?? it just causes issues >,'/

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -7,7 +7,7 @@ CREATE PROCEDURE ComputeAverageScoreForUser(
 )
 BEGIN
     UPDATE users
-    SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id)
+    SET average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
     WHERE id = user_id;
 END;
 //
