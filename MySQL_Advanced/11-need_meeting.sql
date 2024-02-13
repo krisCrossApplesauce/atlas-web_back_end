03031db2ca0bcbc52f1604ddee3072cc838e484f
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-13 15:44:36 -0600

    11: I think that should work ,'P

--- MySQL_Advanced/11-need_meeting.sql
+++ MySQL_Advanced/11-need_meeting.sql
@@ -1,3 +1,7 @@
 -- write an SQL script creates a view named need_meeting
 -- that lists all students that have a score under 80 (strict)
 -- and no last_meeting or more than 1 month
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score <= 80 AND (last_meeting IS NULL OR DATEDIFF(last_meeting, CURDATE()) > 30);
