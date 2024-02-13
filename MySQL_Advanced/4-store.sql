3b62d02c884df740ea3743737ac03adf1f5410e9
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-13 12:29:53 -0600

    4: NEW refers to the new inserted row in orders I think, ON specifies the table it's being triggered by

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,7 +1,9 @@
 -- write an SQL script that creates a trigger
 -- that decreases the quantity of an item after adding a new order.
 -- quantity in the table items can be negative
-CREATE TRIGGER items_trigger AFTER INSERT ON orders
-FOR EACH ROW UPDATE items
-SET quantity = (quantity - orders.number)
-WHERE name = orders.item_name;
+CREATE TRIGGER items_trigger
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items
+SET quantity = (quantity - NEW.number)
+WHERE name = NEW.item_name;
