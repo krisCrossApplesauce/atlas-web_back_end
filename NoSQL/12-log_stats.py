4b4535832a8743657b693f1c0a826caad1eaf282
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-15 16:38:42 -0600

    task 12: I'm guessing this is what I'm supposed to do

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+write a Python script that
+provides some stats about Nginx logs stored in MongoDB:
+   >  Database: logs
+   >  Collection: nginx
+   >  Display (same as the example):
+       >  first line: x logs where x is the number of docs in the collection
+       >  second line: Methods:
+       >  five lines with the number of docs with the
+          method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+       >  one line with the number of docs with:
+           >  method=GET
+           >  path=/status
+"""
+import pymongo
+
+
+if __name__ == "__main__":
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+    my_collection = client.logs.nginx
+
+    print(f"{my_collection.count()} logs")
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        print(f"method {method}: {my_collection.count({"method": [method]})}")
+    print(f"{my_collection.count({
+        "method": "GET",
+        "path": "/status"
+        })} status check")
