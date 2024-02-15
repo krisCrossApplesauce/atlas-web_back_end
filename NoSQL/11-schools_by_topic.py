05f2ba0d46d9c12404010096898e8a279fc5ea7b
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-15 16:12:57 -0600

    11: woops, forgot to return the stuff :P

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -9,4 +9,4 @@ def schools_by_topic(mongo_collection, topic):
     """ I'm pretty sure it should be basically
     the same as searching for something by the name attribute
     """
-    mongo_collection.find({"topics": {"$in": topic}})
+    return mongo_collection.find({"topics": {"$in": topic}})
