04c4221d93a6818bfb34b5281a0aa432cdd29a16
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-15 15:46:57 -0600

    10 :P don't know if all the key values need quotations or not :P

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+write a Python func that changes all topics of a school doc based on name:
+name (string) will be the school name to update
+topics (list of strings) will be the list of topics approached in the school
+"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """ changes topics of a school """
+    mongo_collection.update_one(
+        {name: name},
+        {"$set": {topics: topics}}
+    )
