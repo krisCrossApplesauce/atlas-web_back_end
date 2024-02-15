ca8f468c0c85d3ccd79a150b04e05169c5a04426
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-15 16:11:29 -0600

    11: in is good :)

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+write a Python func that returns the list of school having a specific topic
+"""
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ I'm pretty sure it should be basically
+    the same as searching for something by the name attribute
+    """
+    mongo_collection.find({"topics": {"$in": topic}})
