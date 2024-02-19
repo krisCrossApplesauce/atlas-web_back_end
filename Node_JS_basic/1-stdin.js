e6384e7ce363f3105007f9c8393bb93c662a5771
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-19 16:42:17 -0600

    1: should pass lint now ,'P

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-readline.question(`Welcome to Holberton School, what is your name?\n`, name => {
-    console.log(`Your name is: ${name}`);
+readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  console.log(`Your name is: ${name}`);
 });
 
 readline.on('close', () => {
-    console.log(`This important software is now closing`);
+  console.log('This important software is now closing');
 });
