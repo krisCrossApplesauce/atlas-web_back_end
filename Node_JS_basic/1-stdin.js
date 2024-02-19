1323783cb5b5483ef7888f73aaccc1851f410101
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-19 16:34:01 -0600

    1: now has message when closing program

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,5 +5,8 @@ const readline = require('readline').createInterface({
 
 readline.question(`Welcome to Holberton School, what is your name?\n`, name => {
     console.log(`Your name is: ${name}`);
-    readline.close();
+});
+
+readline.on('close', () => {
+    console.log(`This important software is now closing`);
 });
