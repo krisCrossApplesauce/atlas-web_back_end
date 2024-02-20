4d4109691b1e07491483eb5cd9409a96f6161bd9
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-20 14:59:58 -0600

    2: the database.csv file used in the checker spells the names differently than the database.csv we were given at the beginning of the project

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,23 +1,27 @@
 function countStudents(path) {
   const fs = require('fs');
+  let idx;
+  let i;
+  let ii;
+  let y;
+  let x;
   let data;
   try {
     data = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
-  }
-  catch(err) {
+  } catch (err) {
     throw new Error('Cannot load the database');
   }
 
   const studs = data.split('\n');
   const students = [];
-  for (idx = 0; idx < studs.length - 1; idx++) {
+  for (idx = 0; idx < studs.length - 1; idx += 1) {
     students.push(studs[idx].split(','));
   }
 
   const studObjs = [];
-  for (i = 1; i < students.length; i++) {
+  for (i = 1; i < students.length; i += 1) {
     const temp = {};
-    for (ii = 0; ii < students[0].length; ii++) {
+    for (ii = 0; ii < students[0].length; ii += 1) {
       temp[students[0][ii]] = students[i][ii];
     }
     studObjs.push(temp);
@@ -25,16 +29,15 @@ function countStudents(path) {
 
   console.log(`Number of students: ${studObjs.length}`);
 
-  const distinctFields = [...new Set(studObjs.map(x => x.field))];
+  const distinctFields = [...new Set(studObjs.map((x) => x.field))];
 
-  for (y = 0; y < distinctFields.length; y++) {
-    const studsInField = []
-    for (x = 0; x < studObjs.length; x++) {
-      if (studObjs[x].field == distinctFields[y])
-        studsInField.push(' ' + studObjs[x].firstname);
+  for (y = 0; y < distinctFields.length; y += 1) {
+    const studsInField = [];
+    for (x = 0; x < studObjs.length; x += 1) {
+      if (studObjs[x].field === distinctFields[y]) studsInField.push(` ${studObjs[x].firstname}`);
     }
     console.log(`Number of students in ${distinctFields[y]}: ${studsInField.length}. List:${studsInField}`);
   }
-};
+}
 
 module.exports = countStudents;
