f4e8dadd2cb650fd71f5a9f4ffdabe162f85d8a4
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-21 17:30:44 -0600

    task 3, but require('fs/require') doesn't seem to work so it's basically just the same as my code from task 2

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,6 +5,7 @@ const readline = require('readline').createInterface({
 
 readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
+  readline.close();
 });
 
 readline.on('close', () => {
