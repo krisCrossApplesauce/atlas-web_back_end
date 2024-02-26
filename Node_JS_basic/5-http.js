a3b33570580940aaf4c88c48afed9e15bdbf46d2
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-26 16:17:15 -0600

    5: ok, hopefully it likes it now that I've just straight up copied countStudents into the file

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,5 +1,40 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+  let idx;
+  let i;
+  let ii;
+  let y;
+  let x;
+  const data = await fs.readFile(path, { encoding: 'utf8', flag: 'r' });
+  const studs = data.split('\n');
+  const students = [];
+  for (idx = 0; idx < studs.length - 1; idx += 1) {
+    students.push(studs[idx].split(','));
+  }
+
+  const studObjs = [];
+  for (i = 1; i < students.length; i += 1) {
+    const temp = {};
+    for (ii = 0; ii < students[0].length; ii += 1) {
+      temp[students[0][ii]] = students[i][ii];
+    }
+    studObjs.push(temp);
+  }
+
+  console.log(`Number of students: ${studObjs.length}`);
+
+  const distinctFields = [...new Set(studObjs.map((x) => x.field))];
+
+  for (y = 0; y < distinctFields.length; y += 1) {
+    const studsInField = [];
+    for (x = 0; x < studObjs.length; x += 1) {
+      if (studObjs[x].field === distinctFields[y]) studsInField.push(` ${studObjs[x].firstname}`);
+    }
+    console.log(`Number of students in ${distinctFields[y]}: ${studsInField.length}. List:${studsInField}`);
+  }
+}
 
 const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
