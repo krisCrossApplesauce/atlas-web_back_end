e0cdd5f04e44ff20a4db6bc0df39939a0d23303f
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-26 16:14:53 -0600

    task 5 :P might want me to just straight up copy the func from 3 into the file but it feels unnecessary; conversation topic: tiny homes! :D and stimming/emotional regulation

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,7 +1,14 @@
 const http = require('http');
+const countStudents = require('./3-read_file_async');
 
-const app = http.createServer((req, res) => {
-  res.write('Hello Holberton School!');
+const app = http.createServer(async (req, res) => {
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+  }
+  if (req.url === '/students') {
+    res.write('This is the list of our students');
+    res.write(`${await countStudents(process.argv[2])}`);
+  }
   res.end();
 }).listen(1245);
 
