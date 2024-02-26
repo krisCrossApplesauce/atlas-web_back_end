926355e6999c594a2c90c299a018f02b7f6044a4
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-26 16:32:00 -0600

    3&5: how did I forget the throw Error('Cannot load the database')?? how did 3 pass all checks without it?? ig the checkers weren't checking for it

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -13,32 +13,36 @@ const app = http.createServer(async (req, res) => {
     let ii;
     let y;
     let x;
-    const data = await fs.readFile(path, { encoding: 'utf8', flag: 'r' });
-    const studs = data.split('\n');
-    const students = [];
-    for (idx = 0; idx < studs.length - 1; idx += 1) {
-      students.push(studs[idx].split(','));
-    }
-
-    const studObjs = [];
-    for (i = 1; i < students.length; i += 1) {
-      const temp = {};
-      for (ii = 0; ii < students[0].length; ii += 1) {
-        temp[students[0][ii]] = students[i][ii];
+    try {
+      const data = await fs.readFile(path, { encoding: 'utf8', flag: 'r' });
+      const studs = data.split('\n');
+      const students = [];
+      for (idx = 0; idx < studs.length - 1; idx += 1) {
+        students.push(studs[idx].split(','));
       }
-      studObjs.push(temp);
-    }
 
-    res.write(`Number of students: ${studObjs.length}`);
-
-    const distinctFields = [...new Set(studObjs.map((x) => x.field))];
-
-    for (y = 0; y < distinctFields.length; y += 1) {
-      const studsInField = [];
-      for (x = 0; x < studObjs.length; x += 1) {
-        if (studObjs[x].field === distinctFields[y]) studsInField.push(` ${studObjs[x].firstname}`);
+      const studObjs = [];
+      for (i = 1; i < students.length; i += 1) {
+        const temp = {};
+        for (ii = 0; ii < students[0].length; ii += 1) {
+          temp[students[0][ii]] = students[i][ii];
+        }
+        studObjs.push(temp);
       }
-      res.write(`Number of students in ${distinctFields[y]}: ${studsInField.length}. List:${studsInField}`);
+
+      res.write(`Number of students: ${studObjs.length}`);
+
+      const distinctFields = [...new Set(studObjs.map((x) => x.field))];
+
+      for (y = 0; y < distinctFields.length; y += 1) {
+        const studsInField = [];
+        for (x = 0; x < studObjs.length; x += 1) {
+          if (studObjs[x].field === distinctFields[y]) studsInField.push(` ${studObjs[x].firstname}`);
+        }
+        res.write(`Number of students in ${distinctFields[y]}: ${studsInField.length}. List:${studsInField}`);
+      }
+    } catch (err) {
+      throw new Error('Cannot load the database');
     }
   }
   res.end();
