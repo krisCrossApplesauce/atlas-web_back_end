52d10b2f6067960cba4c1d5e7b4e8d5a03df6d69
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-26 16:24:27 -0600

    5: ok ok ok, fine, I'll integrate it into the if statement or whatever ,'T

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,49 +1,45 @@
 const http = require('http');
 const fs = require('fs').promises;
 
-async function countStudents(path) {
-  let idx;
-  let i;
-  let ii;
-  let y;
-  let x;
-  const data = await fs.readFile(path, { encoding: 'utf8', flag: 'r' });
-  const studs = data.split('\n');
-  const students = [];
-  for (idx = 0; idx < studs.length - 1; idx += 1) {
-    students.push(studs[idx].split(','));
-  }
-
-  const studObjs = [];
-  for (i = 1; i < students.length; i += 1) {
-    const temp = {};
-    for (ii = 0; ii < students[0].length; ii += 1) {
-      temp[students[0][ii]] = students[i][ii];
-    }
-    studObjs.push(temp);
-  }
-
-  console.log(`Number of students: ${studObjs.length}`);
-
-  const distinctFields = [...new Set(studObjs.map((x) => x.field))];
-
-  for (y = 0; y < distinctFields.length; y += 1) {
-    const studsInField = [];
-    for (x = 0; x < studObjs.length; x += 1) {
-      if (studObjs[x].field === distinctFields[y]) studsInField.push(` ${studObjs[x].firstname}`);
-    }
-    console.log(`Number of students in ${distinctFields[y]}: ${studsInField.length}. List:${studsInField}`);
-  }
-}
-
 const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
     res.write('Hello Holberton School!');
   }
   if (req.url === '/students') {
     res.write('This is the list of our students');
-    const students_data = await countStudents(process.argv[2]);
-    res.write(`${students_data}`);
+    const path = process.argv[2];
+    let idx;
+    let i;
+    let ii;
+    let y;
+    let x;
+    const data = await fs.readFile(path, { encoding: 'utf8', flag: 'r' });
+    const studs = data.split('\n');
+    const students = [];
+    for (idx = 0; idx < studs.length - 1; idx += 1) {
+      students.push(studs[idx].split(','));
+    }
+
+    const studObjs = [];
+    for (i = 1; i < students.length; i += 1) {
+      const temp = {};
+      for (ii = 0; ii < students[0].length; ii += 1) {
+        temp[students[0][ii]] = students[i][ii];
+      }
+      studObjs.push(temp);
+    }
+
+    res.write(`Number of students: ${studObjs.length}`);
+
+    const distinctFields = [...new Set(studObjs.map((x) => x.field))];
+
+    for (y = 0; y < distinctFields.length; y += 1) {
+      const studsInField = [];
+      for (x = 0; x < studObjs.length; x += 1) {
+        if (studObjs[x].field === distinctFields[y]) studsInField.push(` ${studObjs[x].firstname}`);
+      }
+      res.write(`Number of students in ${distinctFields[y]}: ${studsInField.length}. List:${studsInField}`);
+    }
   }
   res.end();
 }).listen(1245);
