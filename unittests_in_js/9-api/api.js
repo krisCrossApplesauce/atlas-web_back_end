79e6fc70fba73b035fd38c070a2446806d95c497
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-28 18:38:19 -0600

    9: just copied the stuff, Caramon and Cason are here :)

---
+++ unittests_in_js/9-api/api.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id', (req, res) => {
+  res.send('Payment methods for cart :id');
+});
+
+const server = app.listen(7865, () => {
+  console.log('API available on localhost port 7865')
+});
+
+module.exports = server;
---
+++ unittests_in_js/9-api/api.test.js
@@ -0,0 +1,27 @@
+// const server = require('./api');
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Test server from 8-api/api.js', () => {
+  it('tests GET / and status code', (done) => {
+    request('http://localhost:7865', (err, res, bod) => {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('tests that GET / returns "Welcome to the payment system"', (done) => {
+    request('http://localhost:7865', (err, res, bod) => {
+      expect(bod).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+// to stop the server after the tests should have finished running
+/*
+setTimeout(() => {
+  server.close();
+}, 2000);
+*/
