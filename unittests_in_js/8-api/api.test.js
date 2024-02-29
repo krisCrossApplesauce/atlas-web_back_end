9f5d4322c734bd46b1c3d59b5b1c2370325d0257
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-28 18:21:23 -0600

    8: ok, a lot was wrong with what I was doing, but a fair amount was right, so that's not nothing

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,33 +1,25 @@
-const app = require('./api');
-const sinon = require('sinon');
-const chaiHttp = require('chai-http');
+const server = require('./api');
+const request = require('request');
 const chai = require('chai');
 const expect = chai.expect;
-chai.use(chaiHttp);
 
-describe('Test app from 8-api/api.js', () => {
-  it.skip('tests console.log (when server is started?)', (done) => {
-    const logSpy = sinon.spy(console, 'log');
-    chai.request(app);
-    expect(logSpy.calledWith('API available on localhost port 7865')).to.be.true;
-    expect(logSpy.calledOnce).to.be.true;
-    logSpy.restore();
-  });
-
+describe('Test server from 8-api/api.js', () => {
   it('tests GET / and status code', (done) => {
-    chai.request(app).get('/').end((err, res) => {
-      expect(res.status).to.equal(200);
+    request('http://localhost:7865', (err, res, bod) => {
+      expect(res.statusCode).to.equal(200);
       done();
     });
   });
 
-  it.skip('tests GET / returns "Welcome to the payment system"', (done) => {
-    const resSpy = sinon.spy(app, 'res');
-    chai.request(app).get('/').end((err, res) => {
-      expect(resSpy.calledWith('Welcome to the payment system')).to.be.true;
-      expect(resSpy.calledOnce).to.be.true;
+  it('tests that GET / returns "Welcome to the payment system"', (done) => {
+    request('http://localhost:7865', (err, res, bod) => {
+      expect(bod).to.equal('Welcome to the payment system');
       done();
     });
-    resSpy.restore();
   });
 });
+
+// to stop the server after the tests should have finished running
+const stopServer = setTimeout(() => {
+  server.close();
+}, 2000)
