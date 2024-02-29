37f0e7def934d2e119f06dbe6d3caee8c3499eb7
kris_cross_applesauce <karisrr2203@gmail.com>
2024-02-28 18:03:19 -0600

    8: idk

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,26 +1,33 @@
 const app = require('./api');
 const sinon = require('sinon');
-const assert = require('assert');
+const chaiHttp = require('chai-http');
 const chai = require('chai');
 const expect = chai.expect;
+chai.use(chaiHttp);
 
 describe('Test app from 8-api/api.js', () => {
-  let spy;
-
-  beforeEach(() => {
-    spy = sinon.spy(console, 'log');
+  it.skip('tests console.log (when server is started?)', (done) => {
+    const logSpy = sinon.spy(console, 'log');
+    chai.request(app);
+    expect(logSpy.calledWith('API available on localhost port 7865')).to.be.true;
+    expect(logSpy.calledOnce).to.be.true;
+    logSpy.restore();
   });
 
-  afterEach(() => {
-    spy.restore();
+  it('tests GET / and status code', (done) => {
+    chai.request(app).get('/').end((err, res) => {
+      expect(res.status).to.equal(200);
+      done();
+    });
   });
 
-  it('tests status code', () => {
-    expect(spy.calledWith('API available on localhost port 7865')).to.be.true;
-    expect(spy.calledOnce).to.be.true;
-  });
-
-  it('tests result', () => {
-    assert.equal(0, 0);
+  it.skip('tests GET / returns "Welcome to the payment system"', (done) => {
+    const resSpy = sinon.spy(app, 'res');
+    chai.request(app).get('/').end((err, res) => {
+      expect(resSpy.calledWith('Welcome to the payment system')).to.be.true;
+      expect(resSpy.calledOnce).to.be.true;
+      done();
+    });
+    resSpy.restore();
   });
 });
