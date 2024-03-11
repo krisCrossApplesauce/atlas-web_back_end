83d082ca9dc502fd537683eaeae411acff3a99bd
kris_cross_applesauce <karisrr2203@gmail.com>
2024-03-11 15:57:30 -0500

    0&1: going on a walk now

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,2 +1,28 @@
-const graphql = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = require('graphql');
 
+const TaskType = new GraphQLObjectType({
+  name: 'Type',
+  fields: {
+    id: GraphQLString,
+    title: GraphQLString,
+    weight: GraphQLInt,
+    description: GraphQLString
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    task: {
+      name: TaskType,
+      args: {
+        id: {type: GraphQLString}
+      },
+      resolve(parent, args) {}
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
